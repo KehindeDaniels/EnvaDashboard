6915ae22f976757602fcb9f15ea644f288890d2d
kehindeDaniels <kehindedaniels501@gmail.com>
2024-05-15 14:45:04 +0100

    Responsive side bar

--- src/Components/OverviewCards.jsx
+++ src/Components/OverviewCards.jsx
@@ -4,8 +4,20 @@ const OverviewCards = () => {
     const cards = [0, 1, 2, 3]
     const cardElement = cards.map((index, card)=><div className={`flex-1  h-48 ${index===0 ? 'bg-blue-500' :'bg-white'} shadow  rounded-sm`}></div>)
   return (
-    <div className='flex gap-4'>
-        {cardElement}
+    <div className='flex flex-col gap-8'>
+      <ul className='flex gap-8'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Items</a></li>
+        <li><a href="#">Analytics</a></li>
+        <li><a href="#">Files</a></li>
+      </ul>
+        <div className='flex flex-col gap-4'>
+          <h3 className='text-2xl font-medium'>Overview</h3>
+          <div className=" flex g-8">
+
+          {cardElement}
+          </div>
+        </div>
     </div>
   )
 }
--- src/Components/SidebarArea.jsx
+++ src/Components/SidebarArea.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { faBars, faTachometerAlt, faBox, faShoppingCart, faUsers, faCog, faCalendarAlt, faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 
 const SidebarArea = () => {
-  return (
-    <>
-      <aside className='min-w-64 bg-blue-600 drop-shadow-xl shadow-xl'>
-        
-      </aside>
-    </>
-  )
-}
+  const sideNav = [
+    {
+      navName: 'Sample log',
+      navIcon: faBox,
+    },
+    {
+      navName: 'User',
+      navIcon: faUsers,
+    },
+    {
+      navName: 'Inventory',
+      navIcon: faShoppingCart,
+    },
+    {
+      navName: 'Calender',
+      navIcon: faCalendarAlt,
+    },
+  ]
+  const [collapsed, setCollapsed] = useState(false)
 
-export default SidebarArea
+
+  const navs = sideNav.map((sideNavs, index)=>(
+            <li key={index} className='hover:bg-white hover:bg-opacity-[.13] pl-8 py-4'>
+              <a href="#" className='flex gap-2 items-center'>
+                <FontAwesomeIcon icon={sideNavs.navIcon}/>
+                {!collapsed && <span >{sideNavs.navName}</span>}
+              </a>
+            </li>
+  ))
+
+    function handleCollapsed (){
+    setCollapsed(prev=>!prev)
+    console.log(collapsed)
+  }
+
+
+  return (
+<aside className={`bg-blue-600 drop-shadow-xl shadow-xl min-h-screen transition-all duration-500 ${!collapsed ? 'w-64' : 'w-16'}`}>      <div className="p-8 text-white  flex items-center justify-between gap-8">
+        {!collapsed && <div className="text-2xl font-bold">EnvAccord</div>}
+        <div className="hambugger" onClick={handleCollapsed}>
+          <FontAwesomeIcon icon={faHamburger}/>
+        </div>
+      </div>
+      <nav className=''>
+        <ul className='flex flex-col gap-4 w-full text-white text-lg'>
+          {navs}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default SidebarArea;
