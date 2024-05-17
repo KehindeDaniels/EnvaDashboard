75f9f83612eb7f511cd0d7c5532c722ea4549e94
kehindeDaniels <kehindedaniels501@gmail.com>
2024-05-17 10:29:34 +0100

    Added page folder to repo

---
+++ src/Pages/Consumables.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Consumables = () => {
+  return (
+    <div>Consumables</div>
+  )
+}
+
+export default Consumables
---
+++ src/Pages/SampleLog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SampleLog = () => {
+  return (
+    <div>SampleLog</div>
+  )
+}
+
+export default SampleLog
