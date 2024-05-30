64fa759451e1b2a9a393a8c9031cbc9c6c42a730
kehindeDaniels <kehindedaniels501@gmail.com>
2024-05-30 06:18:47 +0100

    Completed the header's responsivness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { faBell, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faHamburger } from "@fortawesome/free-solid-svg-icons/faHamburger";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import me from "../assets/me";
+import me from "../assets/me.jpg";
 
 const Header = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
@@ -18,39 +18,39 @@ const Header = () => {
         <div className="1st-div flex items-center gap-4">
           <FontAwesomeIcon icon={faHamburger} />
           <div
-            className={`search flex items-center gap-2 text-slate-400 ${
-              !isCollapsed && "border py-1 px-3 w-48 rounded-full"
+            className={`search flex items-center gap-2 text-slate-400 transition-all duration-500 ${
+              !isCollapsed ? "border py-1 px-3 w-48 rounded-full" : "w-8"
             }`}
           >
-            <FontAwesomeIcon
-              icon={faSearch}
-              onClick={toggleSearch}
-            />
+            <FontAwesomeIcon icon={faSearch} onClick={toggleSearch} />
             <input
               type="text"
               placeholder={`${!isCollapsed ? "Search" : ""}`}
-              className="bg-transparent"
+              className="bg-transparent transition-all duration-500"
             />
           </div>
         </div>
 
-        <div className="2nd-div">
+        <div className="2nd-div flex gap-2 items-center justify-center sm:gap-4">
           <div className="2nd-div">
             <div className="notification relative">
-              <FontAwesomeIcon
-                icon={faBell}
-                className="h-6"
-              />
-              <div className="notificationCount absolute bg-red-500 text-white w-6 h-6 rounded-full text-center bottom-4 left-2">
+              <FontAwesomeIcon icon={faBell} className="h-6" />
+              <div className="notificationCount absolute bg-red-500 text-white w-4 h-4 text-xs rounded-full text-center bottom-4 left-2">
                 4
               </div>
             </div>
           </div>
 
-          <div className="profile">
+          <div className="profile flex  items-center justify-center gap-2">
+            <p className="name hidden sm:flex sm:flex-col text-right font-bold ">
+              Kehinde Daniels
+              <span className="text-xs font-normal">Front-end Engineer</span>
+            </p>
+
             <img
               src={me}
-              alt=""
+              alt="profile image"
+              className="w-6 h-6 rounded-full"
             />
           </div>
         </div>
