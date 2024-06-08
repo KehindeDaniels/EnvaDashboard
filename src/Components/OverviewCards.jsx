6dcc72a748001b29e9715821c543c5a5f81be7b2
Kehinde_Daniels <kehindedaniels501@gmail.com>
2024-06-08 20:37:38 +0100

    testing deployment

--- src/Components/OverviewCards.jsx
+++ src/Components/OverviewCards.jsx
@@ -55,7 +55,7 @@ const OverviewCards = () => {
   ));
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {cardElements}
     </div>
   );
