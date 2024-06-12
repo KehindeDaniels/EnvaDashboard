cce769bc51689e0636f67a5d2950d74542ee34b9
Kehinde_Daniels <kehindedaniels501@gmail.com>
2024-06-12 14:18:04 +0100

    installed react table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Layout/Layout";
-import Inventory from "./Layout/Inventory";
-import SampleLog from "./Pages/SampleLog";
-import Clients from "./Pages/Clients";
-import Calender from "./Pages/Calender";
-import Overview from "./Pages/Overview";
-import Items from "./Pages/Items";
-import Consumables from "./Pages/Consumables";
-import Files from "./Pages/Files";
 
 export default function App() {
   return <div className="">Table</div>;
