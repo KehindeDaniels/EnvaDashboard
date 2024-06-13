300bf53bd5317ccf342253edbb3810dbf05b6775
Kehinde_Daniels <kehindedaniels501@gmail.com>
2024-06-13 20:02:43 +0100

    working with grouped data

--- src/Components/BasicTable.jsx
+++ src/Components/BasicTable.jsx
@@ -13,18 +13,31 @@ const BasicTable = () => {
   const columns = useMemo(() => Columns, []);
   const data = useMemo(() => mockData, []);
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
 
   return (
-    <div className="">
-      <table {...getTableProps()}>
+    <div className=" ">
+      <table
+        {...getTableProps()}
+        className="divide-y-4 divide-green-200 mx-auto divide-x-2"
+      >
         {/* Table Head */}
-        <thead>
+        <thead className="bg-green-100">
           {headerGroups.map((hG) => (
-            <tr {...hG.getHeaderGroupProps()} key={hG.id}>
+            <tr {...hG.getHeaderGroupProps()} key={hG.id} className="ml-4 px-6">
               {hG.headers.map((column) => (
-                <th {...column.getHeaderProps()} key={column.id}>
+                <th
+                  {...column.getHeaderProps()}
+                  key={column.id}
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
                   {column.render("Header")}
                 </th>
               ))}
@@ -33,11 +46,38 @@ const BasicTable = () => {
         </thead>
 
         {/* Table Body */}
-        <tbody {...getTableBodyProps()}>
-          <tr>
-            <td></td>
-          </tr>
+        <tbody {...getTableBodyProps()} className="divide-y-2 divide-x-2">
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={row.id}>
+                {row.cells.map((cell) => (
+                  <td
+                    {...cell.getCellProps}
+                    key={cell.id}
+                    className="px-6 py-4 whitespace-nowrap"
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
         </tbody>
+
+        {/* Table Footer */}
+        <tfoot>
+          {footerGroups.map((fG) => (
+            <tr {...fG.getFooterGroupProps()}>
+              {fG.headers.map((column) => (
+                <td {...column.getFooterProps}>
+                  {column.render("Footer")} className="divide-y-4
+                  divide-green-200 mx-auto divide-x-2 bg-green-300"
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
       </table>
     </div>
     // <div className="container mx-auto">
