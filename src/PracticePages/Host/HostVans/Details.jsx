b601709befd07905917372670d6c0d4bd3f3f81d
kehindeDaniels <kehindedaniels501@gmail.com>
2024-06-16 08:11:32 +0100

    no idea

--- src/PracticePages/Host/HostVans/Details.jsx
+++ src/PracticePages/Host/HostVans/Details.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { useOutletContext } from 'react-router-dom'
+import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 const Details = () => {
- const {van} = useOutletContext()
+  const { van } = useOutletContext();
 
   return (
     // <div className='bg-amber-50 h-screen p-8'>
@@ -11,12 +11,17 @@ const Details = () => {
     //         <p className="category mt-1"><span className='font-bold'>Description:</span> {van.description}</p>
     // </div>
     <div className="">
-            <p className="name mt-4"><span className='font-bold'>Name:</span> {van.name}</p>
-            <p className="category mt-1"><span className='font-bold'>Category:</span> {van.type}</p>
-            <p className="category mt-1"><span className='font-bold'>Description:</span> {van.description}</p>
-
+      <p className="name mt-4">
+        <span className="font-bold">Name:</span> {van.name}
+      </p>
+      <p className="category mt-1">
+        <span className="font-bold">Category:</span> {van.type}
+      </p>
+      <p className="category mt-1">
+        <span className="font-bold">Description:</span> {van.description}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
--- src/PracticePages/Vans/VansDetail.jsx
+++ src/PracticePages/Vans/VansDetail.jsx
@@ -1,38 +1,39 @@
-import React, { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import allVans from '../../data'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import allVans from "../../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const VansDetail = () => {
-    const params= useParams()
-    console.log(params)
-    const [vans, setVans] = useState(allVans)
-    const van = vans.find(v=> v.id === params.id)
-        if(!van) {
-            return <div>Van not found</div>
-        }
+  const params = useParams();
+  console.log(params);
+  console.log("did it work");
+  const [vans, setVans] = useState(allVans);
+  const van = vans.find((v) => v.id === params.id);
+  if (!van) {
+    return <div>Van not found</div>;
+  }
   return (
     <>
-    <Link to=".." relative="path">
-        <p className='px-8 underline hover:font-bold'>
-            <FontAwesomeIcon icon={faArrowLeft}/>
-            back to vans
-        </p></Link>
-        <div className="detailContainer p-8">
-            <img src={van.imageUrl} alt={van.name} className='w-80 rounded-lg' />
-            <div className="description">
-                <h3 className='font-bold text-xl'>{van.name}</h3>
-                <p className="details">{van.description}</p>
-                <button className='px-8 py-2 bg-orange-400'>Rent Now</button>
-            </div>
+      <Link to=".." relative="path">
+        <p className="px-8 underline hover:font-bold">
+          <FontAwesomeIcon icon={faArrowLeft} />
+          back to vans
+        </p>
+      </Link>
+      <div className="detailContainer p-8">
+        <img src={van.imageUrl} alt={van.name} className="w-80 rounded-lg" />
+        <div className="description">
+          <h3 className="font-bold text-xl">{van.name}</h3>
+          <p className="details">{van.description}</p>
+          <button className="px-8 py-2 bg-orange-400">Rent Now</button>
         </div>
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default VansDetail
+export default VansDetail;
 
 // import React from 'react';
 // import { useParams } from 'react-router-dom';
